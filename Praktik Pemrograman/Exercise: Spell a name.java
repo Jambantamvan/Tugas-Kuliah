517d0aa9edca2f889e6e0b50b9e0e997017ba2d1
Jambantamvan <abrarkece74@gmail.com>
2025-08-21 09:35:49 +0700

    Update and rename a.txt to Exercise: Spell a name.java

---
+++ Praktik Pemrograman/Exercise: Spell a name.java
@@ -0,0 +1,40 @@
+public class loop {
+    public static void main(String[] args) {
+    a();
+    b();
+    r();
+    a();
+    r();
+    }
+    public static void a() {
+    System.out.println("      A");
+    System.out.println("     A A");
+    System.out.println("    A   A");
+    System.out.println("   A     A");
+    System.out.println("  AAAAAAAAA");
+    System.out.println(" A         A");
+    System.out.println("A           A");
+    System.out.println(" ");
+    }
+    
+    public static void b() {
+    System.out.println("*BBBBBBBBB");
+    System.out.println("*B        B");
+    System.out.println("*B        B");
+    System.out.println("*BBBBBBBBB");
+    System.out.println("*B        B");
+    System.out.println("*B        B");
+    System.out.println("*BBBBBBBBB");
+    System.out.println(" ");
+    } 
+    
+    public static void r() {
+    System.out.println("RRRRRRRRR ");
+    System.out.println("R        R");
+    System.out.println("R        R");
+    System.out.println("RRRRRRRRR ");
+    System.out.println("R      R  ");
+    System.out.println("R       R");
+    System.out.println(" ");
+    }
+}
