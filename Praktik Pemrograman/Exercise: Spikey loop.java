c8e1b93b5f21abebaf483fe6347332d24434700a
Jambantamvan <abrarkece74@gmail.com>
2025-08-21 09:44:58 +0700

    Create Exercise: Spikey loop.java

---
+++ Praktik Pemrograman/Exercise: Spikey loop.java
@@ -0,0 +1,17 @@
+public class loop {
+    public static void main(String[] args) {
+        a();
+        a();
+        a();
+    }
+
+    public static void a() {
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+        System.out.println(" ");
+    }
+}
